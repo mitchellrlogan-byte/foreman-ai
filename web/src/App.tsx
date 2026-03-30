6f0de1d809bd2db3db0e50d36bd7a19f66f53a69
Mitchell Logan <mitchellrlogan@gmail.com>
2026-03-30 10:55:37 -0400

    feat: add Sidebar to App layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Sidebar } from "./components/Sidebar";
 import { Dashboard } from "./pages/Dashboard";
 import { ProjectView } from "./pages/ProjectView";
 import { AddItem } from "./pages/AddItem";
+import { api, type Project } from "./lib/api";
 
 export default function App() {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    api.projects.list().then(setProjects).catch(() => {});
+  }, []);
+
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-gray-50">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/project/:id" element={<ProjectView />} />
-          <Route path="/project/:id/add" element={<AddItem />} />
-        </Routes>
+      <div className="flex min-h-screen bg-[#080f1a]">
+        <Sidebar projects={projects} />
+        <main className="flex-1 overflow-auto">
+          <Routes>
+            <Route path="/" element={<Dashboard onProjectsLoaded={setProjects} />} />
+            <Route path="/project/:id" element={<ProjectView />} />
+            <Route path="/project/:id/add" element={<AddItem />} />
+          </Routes>
+        </main>
       </div>
     </BrowserRouter>
   );
